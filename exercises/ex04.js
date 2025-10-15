c190750dae57d9d9db0bf3923542c2621784c197
qlueir <qarqizhhh8@gmail.com>
2025-10-14 18:51:47 -0700

    boom

---
+++ exercises/ex04.js
@@ -0,0 +1,35 @@
+// button that says "click me"
+//when clicked, button says "Clicked x time"
+//every click is +1 to the x
+//if x is over 1, button changes to "Clicked x times"
+//a top limit of 100
+
+let totalNumber = 0;
+
+$("#needy_button").click(function(){
+ totalNumber = totalNumber + 1;
+ 
+ //idk about this part
+ let sentence = "I was clicked";
+ let totalNumber2 = sentence + totalNumber;
+
+ 
+ $("#needy_button").html("You clicked me " + totalNumber + " times!");
+
+});
+
+$("#cat").click(function(){
+
+    "#cat" = "#boom"
+
+
+
+
+
+})
+
+
+
+
+
+
